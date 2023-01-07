ab0c0fa7b241f96abc2232d7e821a225263aab2c
lewisgilldotcom <59139741+lewisgilldotcom@users.noreply.github.com>
2023-01-07 17:19:24 +0000

    Fixed spelling

--- rsync_latest.sh
+++ rsync_latest.sh
@@ -3,7 +3,7 @@
 #File Name: rsync_latest.sh
 #Author: Lewis Gill -- lewisgill.com
 #Source: https://github.com/lewisgilldotcom/scripts <--License file located here
-#Descriptipn: Will download to your local machine, the latest 3 zip backup files on a remote server running rsync.
+#Description: Will download to your local machine, the latest 3 zip backup files on a remote server running rsync.
 #This script is intended to be used with the Server Backup Spigot plugin (https://www.spigotmc.org/resources/server-backup-world-ftp-backup-1-8-1-19-multithreaded.79320/) for Minecraft and when utilised for the intended use case it will download the latest end, nether and overworld backup to your local machine since Server Backup will always make these 3 backups one after the other. 
 #If you are running a Linux server that uses bash and are on a Linux host you only need to change the SSH_DETAILS and DOWNLOAD_DIR variables. It is recommened to authenticate via SSH Public Keys as rsync will ask for you password a total of four times otherwise.   
 
